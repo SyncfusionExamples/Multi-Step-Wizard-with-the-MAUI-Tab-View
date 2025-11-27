f9abd0a8b217d2fa78f2b1737fa410681df4a0bd
snehakPV <sneha.sf4154@gmail.com>
2025-11-27 11:52:44 +0530

    Prepared the sample and read me file for the Tab View

---
+++ MultiStepForm/MultiStepForm/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MultiStepForm
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
